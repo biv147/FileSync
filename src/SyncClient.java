fefce6f341d2ea3277a57406db8775c0a448fa7b
ICUBE321 <47456033+ICUBE321@users.noreply.github.com>
2020-11-15 00:25:57 -0500

    implemented a method to monitor file's timestamp

--- src/SyncClient.java
+++ src/SyncClient.java
@@ -1,9 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
 
 import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
 public class SyncClient {
+	
+	//for updating server with the new file
+	public void fileUpdate(String from, String to) throws IOException {
+		Path src = Paths.get(from);
+		Path dest = Paths.get(to);
+		Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
+	}
+	
 	public static void main(String[] args) {
 		ZMQ.Context context = ZMQ.context(1);
 		
@@ -20,13 +34,15 @@ public class SyncClient {
         client.subscribe(subscription.getBytes(ZMQ.CHARSET));
         System.out.println("SUB SUCCESS");
         
-        String topic = client.recvStr();
-        if (topic != null) {
-        	String data = client.recvStr();
-            assert (topic.equals(subscription));
-            System.out.println(data);
+        while(true) {
+        	 String topic = client.recvStr();
+             if (topic == null) {
+                 break;
+             }
+             String data = client.recvStr();
+             assert (topic.equals(subscription));
+             System.out.println("Received for gorup: [" + topic + "] File status: " + data);
         }
-		
 		client.close();
 		context.term();
 	}
--- src/SyncServer.java
+++ src/SyncServer.java
@@ -1,7 +1,19 @@
+
+import java.io.File;
+import java.util.Scanner;
+
 import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
 public class SyncServer {
+	long timeStamp;
+	File file;
+	
+	public SyncServer(File file)  {
+		this.file = file;
+		this.timeStamp = file.lastModified();
+	}
+	
 	public static void main(String[] args) throws InterruptedException {
 		
 		ZMQ.Context context = ZMQ.context(1);
@@ -13,13 +25,28 @@ public class SyncServer {
 		int groupNum = 1;
 		
 		server.send(String.format("%d", groupNum), ZMQ.SNDMORE);
-		 while (!Thread.currentThread().isInterrupted()) {
-	            Thread.sleep(500);
-	            String dest = String.format("%d", groupNum);
-	            server.send(dest, ZMQ.SNDMORE);
-	            server.send("Welcome to group "+ groupNum);
+		
+		//setting the first timestamp
+//		System.out.print("Enter your file's address: ");
+//        Scanner file_input = new Scanner(System.in);
+        SyncServer newServer = new SyncServer(new File("C:\\Users\\izien\\Documents\\Distributed Systems\\File-Synchronizer\\src\\sample.txt"));
+        //!Thread.currentThread().isInterrupted()
+		 while (true) {
+			 	Thread.sleep(500);
+			 	long timeStamp = newServer.file.lastModified();
+	        	if(newServer.timeStamp != timeStamp) {
+	        		newServer.timeStamp = timeStamp;
+	        		String data = "new";
+	                String dest = String.format("%d", groupNum);
+	                server.send(dest, ZMQ.SNDMORE);
+	                server.send(data);		
+	        		System.out.println("File's changed!");
+	        	}
+//	            String dest = String.format("%d", groupNum);
+//	            server.send(dest, ZMQ.SNDMORE);
+//	            server.send("Welcome to group "+ groupNum);
 	        }
-		server.close();
-		context.term();
+//		server.close();
+//		context.term();
 	}
 }
